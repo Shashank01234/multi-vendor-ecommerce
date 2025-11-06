e0745cdd098bfdba32540febf29828568fabbeb0
Shashank <spandey21217@gmail.com>
2025-11-06 19:08:47 +0530

    fifth commit

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,48 @@
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
+import { SearchFilters } from "./search-filters";
+import { Category } from '@/payload-types';
 
 interface Props {
     children: React.ReactNode;
 }
 
-const Layout = ({ children }: Props) => {
-    return (
+const Layout = async ({ children }: Props) => {
+    const payload = await getPayload({
+          config: configPromise,
+     });
+
+    const data = await payload.find({
+        collection: 'categories',
+         depth: 1, // Pouplate subcategories, subcategories.[0] will be of type Category
+         pagination: false,
+         where: {
+            parent: {
+                exists: false,
+            },
+        },
+    });
+
+    const formattedData = data.docs.map((doc) => ({
+        ...doc,
+        subcategories: (doc.subcategories ?.docs ??  []).map((doc) => ({
+            // Because of "depth: 1" above, subcategories are populated, we are confident "doc" is of type Category
+            ...(doc as Category),
+            subcategories: undefined, 
+        }))
+    }));
+
+    console.log({
+        data, formattedData
+    });
+
+    return (    
         <div className="flex flex-col min-h-screen">
             <Navbar />
+            <SearchFilters data={formattedData}/>
             <div className="flex-1 bg-[#F4F4F0]">
                 {children}
             </div>
