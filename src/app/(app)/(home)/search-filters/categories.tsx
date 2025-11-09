1bc1d336429b42dc8af283ef189c967e19e3b00c
Shashank <spandey21217@gmail.com>
2025-11-10 03:03:50 +0530

    seventh commit

--- src/app/(app)/(home)/search-filters/categories.tsx
+++ src/app/(app)/(home)/search-filters/categories.tsx
@@ -6,12 +6,13 @@ import { useEffect, useRef, useState } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 
+import { CategoriesGetManyOutput } from "@/modules/categories/types";
+
 import { CategoryDropDown } from "./category-dropdown";
 import { CategoriesSidebar } from "./categories-sidebar";
-import { CustomCategory } from "../types";
 
 interface Props{
-    data: CustomCategory[];
+    data: CategoriesGetManyOutput;
 };
 
 export const Categories = ({ data }: Props) => {
@@ -60,7 +61,7 @@ export const Categories = ({ data }: Props) => {
     return (
         <div className="relative w-full">
 
-            <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen} data={data} />
+            <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
 
             {/* Hidden div to measure all items */}
             <div 
---
+++ src/modules/categories/server/procedures.ts
@@ -0,0 +1,30 @@
+import { Category } from "@/payload-types";
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+
+export const categoriesRouter = createTRPCRouter({
+    getMany: baseProcedure.query(async ({ ctx }) => {
+        const data = await ctx.db.find({
+            collection: 'categories',
+            depth: 1, // Pouplate subcategories, subcategories.[0] will be of type Category
+             pagination: false,
+            where: {
+                parent: {
+                    exists: false,
+                },
+            },
+
+            sort: 'name',
+        });
+
+         const formattedData = data.docs.map((doc) => ({
+                ...doc,
+                subcategories: (doc.subcategories ?.docs ??  []).map((doc) => ({
+                    // Because of "depth: 1" above, subcategories are populated, we are confident "doc" is of type Category
+                    ...(doc as Category),
+                    subcategories: undefined, 
+                }))
+            }));
+
+        return formattedData;
+    }),
+});
