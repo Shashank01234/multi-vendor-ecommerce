6fa7e165a5e1af772481d494472010697158443a
Shashank <spandey21217@gmail.com>
2025-11-16 00:55:02 +0530

    tenth commit

--- src/modules/home/ui/components/search-filters/categories.tsx
+++ src/modules/home/ui/components/search-filters/categories.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ListFilterIcon } from "lucide-react";
+import { useParams } from "next/navigation"
 import { useEffect, useRef, useState } from "react";
 
 import { cn } from "@/lib/utils";
@@ -16,6 +17,8 @@ interface Props{
 };
 
 export const Categories = ({ data }: Props) => {
+    const param = useParams();
+
     const containerRef = useRef<HTMLDivElement>(null);
     const measureRef = useRef<HTMLDivElement>(null);
     const viewAllRef = useRef<HTMLDivElement>(null);
@@ -24,7 +27,8 @@ export const Categories = ({ data }: Props) => {
     const [isAnyHovered, setIsAnyHovered] =  useState(false);
     const [isSidebarOpen, setIsSidebarOpen] =  useState(false);
 
-    const activeCategory = "all";
+    const categoryParam = param.category as string | undefined;
+    const activeCategory = categoryParam || "all";
 
     const activeCategoryIndex = data.findIndex((cat) => cat.slug === activeCategory);
     const isActiveCategoryHidden = activeCategoryIndex >= visibleCount && activeCategoryIndex !== -1;
@@ -99,6 +103,7 @@ export const Categories = ({ data }: Props) => {
 
                 <div ref={viewAllRef} className="shrink-0">
                     <Button 
+                        variant="elevated"
                         className={cn(
                             "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
                             isActiveCategoryHidden && !isAnyHovered && "bg-white border-primary",
